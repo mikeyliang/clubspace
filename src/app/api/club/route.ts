7fb724a2a4ce03cfaa59dbda8f00c9d652dcc9db
Brandon Li <brandonyusenli@gmail.com>
2023-03-11 10:14:07 -0500

    new vite app

--- src/app/api/club/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest } from "next/server";
-import prisma from "../client";
-
-export async function GET(req: NextRequest) {
-  prisma.$queryRaw`SELECT * FROM club`;
-  return;
-}
--- src/app/api/user/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import prisma from "../client";
-
-export async function GET() {
-  const data = await prisma.$queryRaw`SELECT * FROM "User"`;
-  return NextResponse.json(data);
-}
